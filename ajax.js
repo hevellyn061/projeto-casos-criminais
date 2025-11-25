d974e45d7bb58cca221cc920441c90500730042d
hevellyn061 <hevellynbruna2009@gmail.com>
2025-11-25 17:25:19 +0000

    subindo ajax

---
+++ ajax.js
@@ -0,0 +1,27 @@
+const url = "https:/jsonplaceholder.typicode.com/todos/1";
+const output = document.getElementById("output");
+const status = document.getElementById("status");
+function setLoanding(on){
+status.innerHTML = on
+? 'Carregando <span class="spinner" aria-hidden="true"></span>'
+:"";
+}
+document
+ .getElementById("btn-fetch")
+ .addEventListener("click", async () => {
+    output.textContent = "";
+    setLoanding(true);
+    try{
+        const res = await fetch(url, { method: "GET"});
+        if (!res.ok) throw new Error("Status HTTP : " + res.status);
+        const data = await res.json(); 
+        output.textContent = JSON.stringify(data, null, 2)
+    } catch (err) {
+        output.textContent = "Erro: " + err.message;
+        output.classList.add("error");
+    }
+    finally{
+        setLoanding(false);
+    }
+ });
+
